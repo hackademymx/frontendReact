3b6df4a2e74c798ed5e27db9f41933909d94b5fc
JordiEspinozaMendoza <jordi8101@gmail.com>
2022-03-01 20:09:43 -0800

    Mentoria 1 Marzo

--- src/App.js
+++ src/App.js
@@ -4,45 +4,83 @@ import React, { useEffect, useState } from "react";
 import { Boton } from "./components/Boton";
 // Importación de hojas de estilos
 import "./App.css";
-// Importación de imagen
-// import image from "../../image.jpg";
 
+let urlApi = "https://jsonplaceholder.typicode.com/users";
 // * Función que renderiza nuestra app
 function App() {
-  // * JSX
-  // * Javascript en HTML
-  // ! Class es una palabra reservada de Javascript
-  // ? Hooks son funciones que vienen con React
+  // Cargando la petición a la API
+  const [loading, setLoading] = useState(false);
+  // Error
+  const [error, setError] = useState(false);
+  // Data
+  const [data, setData] = useState(null);
+  // Función FETCH
+  // Función que llama a una API (Backend)
+  // nombreFuncion = () => {}
 
-  // * Use state -> Hook
-  const [contador, setContador] = useState(0);
-  const [nombre, setNombre] = useState("");
-  // * UseEffect -> Hook
-  useEffect(() => {
-    // Ejecuta esto si cambian las variables
-    setContador(100);
-  }, [nombre]);
+  // Async -> Función asincrona
+  // await -> esperar
+  const getUsers = async () => {
+    try {
+      // Empezamos la peticion y mostramos la pantalla de carga
+      setError(false); // Si teniamos algún error previamente
+      setLoading(true); // Activamos la pantalla de carga
+
+      // Todo el codigo que puede romperse o mandar error
+
+      // Llamar al backend o API
+      let response = await fetch(urlApi);
+
+      //Recabar la información
+      let data = await response.json();
+
+      // Si todo pasa correctamente y no hubo error quitamos
+      // la pantalla de carga y guardamos la data
+      setLoading(false);
+      setData(data);
+    } catch (error) {
+      // Es el codigo que se ejecutará cuando ocurra algún error
+      setLoading(false);
+      setError(true);
+    }
+  };
+  const returnUsers = () => {
+    return (
+      <div className="users__box">
+        {data?.map((item, index) => (
+          <div className="user__card" key={index}>
+            <span>{item.name}</span>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  // useEffect(() => {
+  //   getUsers();
+  // }, []);
   return (
     <div className="App" id="App">
-      <div>El valor de mi contador es: {contador}</div>
-      <button
-        onClick={() => {
-          // Llamamos al hook y le asignamos otro valor al contador
-          setContador(contador + 1);
-        }}
-      >
-        Aumentar contador
-      </button>
-      <input
-        value={nombre}
-        onChange={(event) => {
-          setNombre(event.target.value);
-        }}
-        placeholder="Ingresa tu nombre"
-      />
-      <Boton parametro1={"Hola soy el parametro"}/>
-      <span>Mi nombre es: {nombre}</span>
-      <p>Mi nombre es: {nombre}</p>
+      <h1>Control de estados</h1>
+      <Boton text="Cargar datos" onClick={getUsers} />
+      {loading === true && (
+        <img
+          src={"https://c.tenor.com/YxE9mubQz_8AAAAS/kermit-typing.gif"}
+          alt="Cargando"
+        />
+      )}
+      {/* {data !== null && (
+        <img
+          src={"https://c.tenor.com/0-JMVRIa4gUAAAAC/kermit-happy.gif"}
+          alt="Todo salio bien"
+        />
+      )} */}
+      {error === true && (
+        <img
+          src={"https://c.tenor.com/RrWO6AAuCQ4AAAAC/scared-nervous.gif"}
+          alt="Error"
+        />
+      )}
+      {returnUsers()}
     </div>
   );
 }
--- src/components/Boton/index.js
+++ src/components/Boton/index.js
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import "./styles.css";
-export function Boton({ parametro1 }) {
-  const [textoBoton, setTextoBoton] = useState("Soy un boton");
+export function Boton({ text, onClick }) {
   return (
-    <button
-      className="boton-componente"
-      onClick={() => {
-        setTextoBoton("Soy Jordi");
-      }}
-    >
-      {parametro1}
+    <button className="boton-componente" onClick={onClick}>
+      {text}
     </button>
   );
 }
